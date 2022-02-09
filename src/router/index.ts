b0843d3e5351d9c1c78be9d752541ab5f3c108a5
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-02-09 11:37:08 +0900

    管理者情報を登録するファイルの作成（空）、ルーターに登録。

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,11 @@ const routes: Array<RouteConfig> = [
     name: "",
     component: () => import("../views/aaa.vue"),
   },
+  {
+    path: "/registerAdmin",
+    name: "",
+    component: () => import("../views/registerAdmin.vue"),
+  },
 ];
 
 const router = new VueRouter({
